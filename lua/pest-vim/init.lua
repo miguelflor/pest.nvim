aefa473ffd930e49dfc282d26a6c0eaa409aa672
Miguel Cid Flor <cidflor2004@gmail.com>
2026-03-19 12:52:12 +0000

    fix: Colors where not attaching

--- lua/pest-vim/init.lua
+++ lua/pest-vim/init.lua
@@ -4,6 +4,8 @@ function M.setup(opts)
   -- Doing the lsp setup here can interfere with the lsp configuration of the user
   -- vim.lsp.config("pest_ls", opts)
   -- vim.lsp.enable("pest_ls")
+  local plugin_path = vim.fn.fnamemodify(debug.getinfo(1, "S").source:sub(2), ":h:h:h")
+
 
   vim.api.nvim_create_autocmd("User", {
     pattern = "TSUpdate",
@@ -13,9 +15,17 @@ function M.setup(opts)
           url      = "https://github.com/miguelflor/tree-sitter-pest",
           revision = "b23fd07600575dad6bb6a11df7c3bb5eed122f1d",
           files    = { "src/parser.c" },
+          queries  = plugin_path .. "/queries/pest"
         },
         filetype = "pest",
       }
+    end,
+  })
+
+  vim.api.nvim_create_autocmd("FileType", {
+    pattern = "pest",
+    callback = function()
+      vim.treesitter.start()
     end,
   })
 
