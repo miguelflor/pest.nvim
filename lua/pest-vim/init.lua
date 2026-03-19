a9da750be617e7922f761c6ec4acb7caf59efd42
Miguel Cid Flor <cidflor2004@gmail.com>
2026-03-19 12:10:32 +0000

    fix: Changing the code to the setup() and changing the installation of the treesitter to the correct way

--- lua/pest-vim/init.lua
+++ lua/pest-vim/init.lua
@@ -4,6 +4,41 @@ function M.setup(opts)
   -- Doing the lsp setup here can interfere with the lsp configuration of the user
   -- vim.lsp.config("pest_ls", opts)
   -- vim.lsp.enable("pest_ls")
+
+  vim.api.nvim_create_autocmd("User", {
+    pattern = "TSUpdate",
+    callback = function()
+      require("nvim-treesitter.parsers").pest = {
+        install_info = {
+          url      = "https://github.com/miguelflor/tree-sitter-pest",
+          revision = "b23fd07600575dad6bb6a11df7c3bb5eed122f1d",
+          files    = { "src/parser.c" },
+        },
+        filetype = "pest",
+      }
+    end,
+  })
+
+  local ok, devicons = pcall(require, "nvim-web-devicons")
+  if not ok then return end
+
+  devicons.set_icon({
+    pest = {
+      icon = "",
+      color = "#F5A623",
+      cterm_color = "214",
+      name = "Pest",
+    },
+  })
+
+  local ok, mini_icons = pcall(require, "mini.icons")
+  if not ok then return end
+
+  mini_icons.setup({
+    extension = {
+      pest = { glyph = "", hl = "MiniIconsYellow" },
+    },
+  })
 end
 
 return M
